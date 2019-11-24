d28f877083010abe8565e258d548b8252125cc6c
Jed Smit <jedsmit1@gmail.com>
2019-11-24 12:14:27 -0800

    Added axios call for movie-this option

--- liri.js
+++ liri.js
@@ -21,6 +21,8 @@ function concertThis() {
         let data = response.data
         let date = moment(data[0].datetime).format("MM-DD-YYYY")
         console.log("Venue: " + data[0].venue.name + "\nLocation: " + data[0].venue.city + ", " + data[0].venue.region + "\nDate: " + date)
+    }).catch(error => {
+        console.log(error);
     });
 };
 
@@ -32,7 +34,9 @@ function spotifyThisSong() {
     let song = searchTerm;
     spotify.search({ type: "track", query: song }).then(response => {
         console.log(response.tracks.items[0].album)
-    })
+    }).catch(error => {
+        console.log(error);
+    });
 };
 
 //movie-this gets title, year, imdb rating,rotten tomatoes rating, country of production, language, plot, and actors
@@ -41,8 +45,17 @@ function spotifyThisSong() {
 
 function movieThis() {
     let movie = searchTerm;
-
+    let movieUrl = "http://www.omdbapi.com/?apikey=715b0924&t=" + movie
+    axios.get(movieUrl).then(response => {
+        let data = response.data;
+        console.log("Title: " + data["Title"] + "\nYear: " + data["Year"] + "\nIMDB Rating: " + data["imdbRating"] + "\nRotten Tomatoes Rating: " + data["Ratings"][1]["Value"] + "\nCountry of Production: " + data["Country"] + "\nLanguage: " + data["Language"] + "\nPlot: " + data["Plot"]
+            + "\nActors: " + data["Actors"]);
+    }).catch(error => {
+        console.log(error);
+    });
 }
+
+
 
 if (action === "concert-this") {
     concertThis();
